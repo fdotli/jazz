1a42270d48b6165418583aca35909cce2533a87e
Guillaume Cartier <gcartier@jazzscheme.org>
2018-07-12 18:46:22 -0400

    Update to latest freetype / png on mac

--- lib/jazz.freetype/foreign/mac/freetype/copy.sh
+++ lib/jazz.freetype/foreign/mac/freetype/copy.sh
@@ -8,8 +8,8 @@ if [ -d lib ]; then
   rm -r lib
 fi
 
-cp -r /Users/cartier/Devel/local/include/freetype2 include
+cp -r /usr/local/Cellar/freetype/2.9.1/include/freetype2 include
 
 mkdir lib
-cp /Users/cartier/Devel/local/lib/libfreetype.6.dylib lib/libfreetype.6.dylib
+cp /usr/local/Cellar/freetype/2.9.1/lib/libfreetype.6.dylib lib/libfreetype.6.dylib
 chmod 755 lib/libfreetype.6.dylib
