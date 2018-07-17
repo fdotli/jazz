5c16eb84cadc9536659b7ef8ba6dc2e41bf723f2
Guillaume Cartier <gcartier@jazzscheme.org>
2018-07-17 09:09:46 -0400

    Revert "Update to latest freetype / png on mac"
    This reverts commit 1a42270d48b6165418583aca35909cce2533a87e.

--- lib/jazz.cairo/foreign/mac/png/copy.sh
+++ lib/jazz.cairo/foreign/mac/png/copy.sh
@@ -8,8 +8,8 @@ if [ -d lib ]; then
   rm -r lib
 fi
 
-cp -r /usr/local/Cellar/libpng/1.6.34/include/libpng16 include
+cp -r /Users/cartier/Devel/local/include/libpng16 include
 
 mkdir lib
-cp /usr/local/Cellar/libpng/1.6.34/lib/libpng16.16.dylib lib
+cp /Users/cartier/Devel/local/lib/libpng16.16.dylib lib
 chmod 755 lib/libpng16.16.dylib
