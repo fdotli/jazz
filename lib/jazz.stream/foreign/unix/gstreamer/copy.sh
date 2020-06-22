6f786a1730a533c229167a2c5c2f50cfdfff6982
Frederic Hamel <frederic.hamel123@gmail.com>
2020-06-22 17:26:10 -0400

    replace unix gstreamer library with the version in ubuntu 20.04

--- lib/jazz.stream/foreign/unix/gstreamer/copy.sh
+++ lib/jazz.stream/foreign/unix/gstreamer/copy.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-# Don't commit the following line
-GSTREAMER=$PWD/gst-build-1.16.2/pkg/usr/local
-GSTREAMER_LIB="$GSTREAMER/lib"
+GSTREAMER=/usr
+GSTREAMER_LIB="$GSTREAMER/lib/x86_64-linux-gnu"
 
 if [ -d gstreamer ]; then
   rm -r gstreamer
@@ -22,26 +21,8 @@ cpplugin() {
 }
 
 cpshared() {
-    if test -r $GSTREAMER_LIB/$1.$2; then
-      echo "Copying $1.$2 from $GSTREAMER_LIB"
-      cp $GSTREAMER_LIB/$1.$2 gstreamer/lib/$1.$2
-    else
-      for lib in $GSTREAMER_LIB/$1.*; do
-        if test -r "$lib"; then
-          echo "*** Found version $lib"
-          exit 1
-        fi
-      done
-
-      if test -r /usr/lib/$1.$2; then
-        echo "Copying $1.$2 from /usr/lib"
-        cp /usr/lib/$1.$2 gstreamer/lib/$1.$2
-      else
-        echo "Failed copying shared library $1.$2"
-        exit 1
-      fi
-    fi
-
+    test -r $GSTREAMER_LIB/$1.$2 && echo Copying shared $1.$2 || echo Failed shared $1.$2
+    cp $GSTREAMER_LIB/$1.$2 gstreamer/lib/$1.$2
     patchelf --set-rpath '$ORIGIN' gstreamer/lib/$1.$2
     ln -sr gstreamer/lib/$1.$2 gstreamer/lib/$1
 }
@@ -83,8 +64,6 @@ cpplugin gstreamer-1.0/libgstwavparse
 cpplugin gstreamer-1.0/libgstx264
 cpplugin gstreamer-1.0/libgstximagesrc
 
-cpshared libc.so 6
-cpshared libdl.so 2
 cpshared libffi.so 7
 cpshared libgio-2.0.so 0
 cpshared libglib-2.0.so 0
@@ -105,13 +84,13 @@ cpshared libgstrtp-1.0.so 0
 cpshared libgsttag-1.0.so 0
 cpshared libgstvideo-1.0.so 0
 cpshared libjpeg.so 8
-cpshared libm.so 6
+cpshared liblzma.so 5
 cpshared libogg.so 0
-cpshared libpcre.so 1
 cpshared libpng16.so 16
 cpshared libvorbis.so 0
 cpshared libvorbisenc.so 2
-cpshared libx264.so 159
+cpshared libx264.so 155
+cpshared libbz2.so 1.0
 cpshared libz.so 1
 
 
@@ -121,7 +100,7 @@ cpshared libz.so 1
 
 mkdir gstreamer/libexec
 mkdir gstreamer/libexec/gstreamer-1.0
-cp -v $GSTREAMER/libexec/gstreamer-1.0/gst-plugin-scanner gstreamer/libexec/gstreamer-1.0
+cp -v $GSTREAMER_LIB/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner gstreamer/libexec/gstreamer-1.0
 patchelf --set-rpath '$ORIGIN/../../lib' gstreamer/libexec/gstreamer-1.0/gst-plugin-scanner
 
 
@@ -134,10 +113,10 @@ mkdir gstreamer/include
   cp -r $GSTREAMER/include/gstreamer-1.0 gstreamer/include || \
   echo 'Failed copying gstreamer include files'
 
-[ -r /usr/include/glib-2.0 ] && \
-  cp -r /usr/include/glib-2.0 gstreamer/include || \
+[ -r $GSTREAMER/include/glib-2.0 ] && \
+  cp -r $GSTREAMER/include/glib-2.0 gstreamer/include || \
   echo 'Failed copying glib-2.0 include files'
 
-[ -r /usr/lib/glib-2.0 ] && \
-  cp -r /usr/lib/glib-2.0 gstreamer/lib || \
+[ -r $GSTREAMER_LIB/glib-2.0 ] && \
+  cp -r $GSTREAMER_LIB/glib-2.0 gstreamer/lib || \
   echo 'Failed copying glib-2.0 lib files'
